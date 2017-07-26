307ff131dd68536d7909be05e51580053f97336c
spstpl <sandeep@systango.com>
2017-07-26 18:42:11 +0530

    Inserted Spinner on SplashScreen

--- js/components/SplashScreen/index.js
+++ js/components/SplashScreen/index.js
@@ -1,25 +1,41 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
+import styles from './style';
 
-class SplashScreen extends Component{
+export default class SplashScreen extends Component{
 
   constructor(props){
     super(props);
+    console.log("in constructor","");
     this.state={
-      isFetching: false,
+      isFetching: true,
     }
   }
 
   componentDidMount(){
-    //call api to fetch user data....
+    //call api to fetch user data 
+    //2. isUserlogin ? fastforward to home : LoginScreen (async Storage.)
   }
 
   componentWillReceiveProps(nextProps){
-    //verify fetching and go to primary screen.
+    //verify fetching and go to next screen accordingly.
   }
 
   render(){
+
+    // Loader to be stopped when user data is loaded....
+    // if user is not already logged in, then the screen should stay for 4-5 seconds and,
+    // then navigate to lOGIN/Signp Screen.
+    
+    return(
     <View style={styles.containerView}>
+      <ActivityIndicator
+        animating={this.state.isFetching}
+        size="large"
+        color="#fff"
+        hidesWhenStopped={true}/>
     </View>
+    );
+
   }
 }
