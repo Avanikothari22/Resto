5f2e2e715aadcd31759d3a2d21f38f21b2a5ddd1
spstpl <sandeep@systango.com>
2017-08-04 20:42:09 +0530

    Redux store created

--- js/configureStore.js
+++ js/configureStore.js
@@ -1,2 +1,16 @@
-import { createStore } from  'redux';
+import { createStore, applyMiddleware, compose } from  'redux';
+import { AsyncStorage } from 'react-native';
+import reducer from './reducers';
+import {persistStore} from 'redux-persist';
+import thunk from 'redux-thunk';
+import { persistStore, autoRehydrate } from 'redux-persist';
 
+export default function configureStore(onCompletion) {
+    const enhancer = compose(
+        applyMiddleware(
+            thunk,
+        ), 
+    );
+    const store = createStore(reducer, enhancer);
+    persistStore(store, {storage: AsyncStorage}, onCompletion);
+}
