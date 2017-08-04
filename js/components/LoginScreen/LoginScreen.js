bfc44b105add4013e4c68843a808453016b43f6b
surajj2223 <23surajj95@gmail.com>
2017-08-04 09:35:52 +0530

    LoginScreen designed

--- js/components/LoginScreen/LoginScreen.js
+++ js/components/LoginScreen/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {TextInput, Image, Button, View} from 'react-native';
+import {TextInput, Image, Button, View, ScrollView, Text, TouchableOpacity} from 'react-native';
 
 import styles from './style';
 
@@ -21,37 +21,61 @@ export default class LoginScreen extends Component{
 
     render(){
         return(
-        <View style={styles.container}>
-            <View style={styles.logoView}>
-                <Image style={styles.logo} source={require('../SplashScreen/assets/my_logo.png')}/>
-            </View>
-            <View style={[styles.container,{padding:16, justifyContent:'space-around'}]}>
-                <View style={{flex:2, justifyContent: 'space-around'}}>
-                    <View style={styles.inputBox}>
-                        <Image style={{flex:1, resizeMode: 'contain', alignSelf: 'center',}} source={require('./assets/email/email.android.png')}/>
-                        <TextInput
-                            onChangeText={(text)=>this.setState({userName: text})}
-                            value={this.state.userName}
-                            style={{borderBottomColor: '#0f163a', flex: 8, alignSelf: 'flex-end', marginLeft:10}}
-                        />
-                        </View>
-                    <View style={styles.inputBox}>
-                       <Image style={{flex:1, resizeMode: 'contain', alignSelf: 'center',}} source={require('./assets/key/key.android.png')}/>
-                        <TextInput
-                            secureTextEntry={true}
-                            onChangeText={(text)=>this.setState({password: text})}
-                            value={this.state.password}
-                            style={{borderBottomColor: '#0f163a', flex: 8, alignSelf: 'flex-end', marginLeft:10}}
-                        />
-                        </View>
+       
+            <View style={styles.container}>
+        
+                <View style={styles.logoView}>
+                    <Image style={styles.logo} source={require('../SplashScreen/assets/my_logo.png')}/>
+                    <View>
+                        <Text style={{color: '#ffffff', fontSize: 16, marginTop: -20}}>Search. Eat. Share.</Text>
+                    </View>
                 </View>
-                <View style={{flex:1, marginTop: 20}}>
-                    <Button
-                        title='Login'
-                        onPress={()=>console.log("loginScreen button pressed !")}/>
+                
+                <View style={styles.welcomeTextView}>
+                    <Text style={styles.welcomeText}>Welcome Back. Login to your Account ! </Text>    
                 </View>
+                
+                <View style={styles.loginContainer}>
+
+                        <View style={styles.inputBox}>
+                            <Image
+                                style={styles.loginIcon}
+                                source={require('./assets/email/email.android.png')}/>
+                            <TextInput
+                                underlineColorAndroid ='#ffffff'
+                                placeholder='Enter your username.'
+                                onChangeText={(text)=>this.setState({userName: text})}
+                                value={this.state.userName}
+                                style={styles.inputTextField}
+                            />
+                        </View>
+                        
+                        <View style={styles.inputBox}>
+                            <Image
+                                style={styles.loginIcon}
+                                source={require('./assets/key/key.android.png')}/>
+                            <TextInput
+                                underlineColorAndroid ='#ffffff'
+                                placeholder='Enter your password.'
+                                secureTextEntry={true}
+                                onChangeText={(text)=>this.setState({password: text})}
+                                value={this.state.password}
+                                style={styles.inputTextField}
+                            />
+                        </View> 
+                        
+                        <TouchableOpacity style={{backgroundColor: '#336666', padding: 16, marginTop:10, borderRadius: 30}}>
+                            <Text style={{color:'#ffffff', fontSize: 18, textAlign: 'center'}}>Login In</Text>
+                        </TouchableOpacity>
+            
+                </View>
+                
+                <View style={{flex:2,}}>
+
+                </View>
+    
             </View>
-        </View>
+      
         );
         
     }
--- js/components/LoginScreen/style.js
+++ js/components/LoginScreen/style.js
@@ -4,12 +4,15 @@ const height = Dimensions.get("window").height;
 const width = Dimensions.get("window").width;
 
 export default styles = StyleSheet.create({
+    
     container:{
-        flex: 1,
-        backgroundColor: '#ffffff',
+        flex:1,
+      backgroundColor: '#0f163a',
+      borderColor: '#ffffff',
+      borderWidth:1,
     },
     logoView:{
-        flex:2,
+        flex:3,
         backgroundColor: '#0f163a',
         justifyContent: 'center',
         alignItems: 'center',
@@ -22,9 +25,46 @@ export default styles = StyleSheet.create({
     inputBox:{
         flexDirection: 'row',
         borderColor: '#D3D3D3',
-        borderRadius: 20,
-       // borderWidth: 1,
+        borderRadius: 30,
+        borderWidth: 1,
         paddingRight: 10,
-        
+        paddingLeft: 10,
+        borderColor:'#ffffff',
+        marginBottom: 10,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputTextField: {
+        borderBottomColor: '#0f163a',
+        flex: 8,
+        alignSelf: 'center',
+        marginLeft:10,
+        marginRight: 10,
+        fontSize: 16,
+    },
+    loginContainer:{
+        flex:3,
+        padding:16,
+        marginTop:10,
+        justifyContent:'center',
+        alignContent:'center',
+    },
+    loginIcon:{
+        flex:1,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+    },
+    welcomeTextView:{
+        flex:1,
+        paddingLeft: 16,
+        paddingRight: 16,
+        justifyContent: 'flex-end',
+        alignItems:'center',
+    },
+    welcomeText:{
+        color: '#ffffff',
+        fontSize: 20,
     }
+    
 });
