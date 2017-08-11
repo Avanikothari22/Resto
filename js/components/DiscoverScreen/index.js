9b9b73151766978d5b4b1f13444110ab2d13c4af
spstpl <sandeep@systango.com>
2017-08-11 18:29:23 +0530

    Discover Screen Work initiated

---
+++ js/components/DiscoverScreen/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import DiscoverScreen from './DiscoverScreen';
+
+export default class Discover extends Component {
+    
+    constructor(props){
+        super(props);
+    }
+s
+    render(){
+        return(
+            <DiscoverScreen />
+        );
+    }
+}
